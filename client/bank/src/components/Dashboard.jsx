9ac9dc00a2125c7e22abf4ca520ebd569acc80e9
reapedjuggler <tomarvibhav55@gmail.com>
2021-10-02 19:09:45 +0530

    Refactored and added custom errors in routes of /kyc
    Signed-off-by: reapedjuggler <tomarvibhav55@gmail.com>

--- client/bank/src/components/Dashboard.jsx
+++ client/bank/src/components/Dashboard.jsx
@@ -1,42 +1,46 @@
-import React, { useEffect, useState } from 'react';
-import Card from '../elements/Card';
-import { url } from '../util/data';
+import React, { useEffect, useState } from "react";
+import Card from "../elements/Card";
+import { url } from "../util/data";
 
-export default function Dashboard({approved}) {
+export default function Dashboard({ approved }) {
+	const [data, setdata] = useState([]);
 
-    const [data, setdata] = useState([]);
+	useEffect(() => {
+		fetch(`${url}/kyc/getapprovals`, {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({
+				bank: "A",
+			}),
+		})
+			.then(response => response.json())
+			.then(res => {
+				if (res.success) {
+					approved
+						? setdata(res.message.approved)
+						: setdata(res.message.pending);
+				}
+			});
+	}, [approved]);
 
-    useEffect(() => {
-        fetch(`${url}/kyc/getapprovals`,{
-          method:'POST',
-          headers:{'Content-Type':'application/json'},
-          body: JSON.stringify({
-            bank:"A"
-          })
-      }).then(response => response.json())
-      .then(res => {
-          if(res.success) {
-            approved?setdata(res.message.approved):setdata(res.message.pending);
-          }
-      })
-      }, [approved])
-
-    return (
-        <div>
-            <h1 className="text-gray-800 text-center p-4">{approved?'Approved Users':'Pending Approvals'}</h1> 
-            <div>
-                {
-                    data.length <= 0 ? <h2 className="text-gray-600 text-center">No data available</h2>
-                    :
-                    data.map((val,k)=>{
-                       return( 
-                        <div className="flex justify-center">
-                            <Card data={val} key={k}/>
-                        </div>
-                       )
-                    })
-                }
-            </div>
-        </div>
-    )
+	return (
+		<div>
+			<h1 className="text-gray-800 text-center p-4">
+				{approved ? "Approved Users" : "Pending Approvals"}
+			</h1>
+			<div>
+				{data.length <= 0 ? (
+					<h2 className="text-gray-600 text-center">No data available</h2>
+				) : (
+					data.map((val, k) => {
+						return (
+							<div className="flex justify-center">
+								<Card data={val} key={k} />
+							</div>
+						);
+					})
+				)}
+			</div>
+		</div>
+	);
 }
