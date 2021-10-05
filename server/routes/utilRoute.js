522dbdd03c4c8ec333084e228186fe5b2fac4392
reapedjuggler <tomarvibhav55@gmail.com>
2021-10-05 22:51:13 +0530

    Fixes in /utils/getdetails
    Signed-off-by: reapedjuggler <tomarvibhav55@gmail.com>

--- server/routes/utilRoute.js
+++ server/routes/utilRoute.js
@@ -25,30 +25,38 @@ router.post("/getuserdetails", async (req, res) => {
 	try {
 		let resp = await utilService.findByEmail(req.body.email, bankModel);
 
-		let resp1 = {
-			name: resp.name,
-			ifsc_code: resp.ifsc_code,
-			email: resp.email,
-			createdAt: resp.createdAt,
-			_id: resp._id,
-		};
-
-		if (resp != null && Object.keys(resp1).length) {
-			res.send({ success: true, message: resp1 });
-		} else {
-			resp = await utilService.findByEmail(req.body.email, userModel);
-
-			resp1 = {
+		if (resp != undefined && resp != null && Object.keys(resp).length) {
+			let resp1 = {
 				name: resp.name,
+				ifsc_code: resp.ifsc_code,
 				email: resp.email,
 				createdAt: resp.createdAt,
 				_id: resp._id,
 			};
 
-			if (resp != null) {
-				res.send({ success: true, message: resp1 });
+			console.log(resp1);
+
+			res.send({ success: true, message: resp1 });
+		} else {
+			resp = await utilService.findByEmail(req.body.email, userModel);
+
+			if (resp == {}) {
+				res.send({ success: false, message: "Error in /util/getuserdetails" });
 			} else {
-				res.send({ success: false, message: "No user exist" });
+				let resp1 = {
+					name: resp.name,
+					email: resp.email,
+					createdAt: resp.createdAt,
+					_id: resp._id,
+				};
+
+				console.log(resp1);
+
+				if (resp != null) {
+					res.send({ success: true, message: resp1 });
+				} else {
+					res.send({ success: false, message: "No user exist" });
+				}
 			}
 		}
 	} catch (err) {
