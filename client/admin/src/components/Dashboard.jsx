721396624bc993db4ded40897e972a00851734e6
Pranav Agarwal <p2012agarwal@gmail.com>
2021-10-06 01:25:12 +0530

    admin ui

---
+++ client/admin/src/components/Dashboard.jsx
@@ -0,0 +1,143 @@
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { url } from '../util/data';
+import Loading from '../shared/Loading';
+
+export default function Dashboard() {
+
+    const history = useHistory();
+
+    const [email] = useState(localStorage.getItem("email"));
+    const [isloading, setisloading] = useState(false);
+    const [iserror, setiserror] = useState(false);
+    const [bank, setbank] = useState("");
+    const [value, setvalue] = useState("");
+
+    const [data, setdata] = useState([]);
+
+    useEffect(() => {
+        if (!email) return;
+        setisloading(true);
+        fetch(`${url}/kyc/getalltrackingdetails`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                email: email
+            })
+        }).then(response => response.json())
+            .then(res => {
+                if (res.success) {
+                    setdata(res.message);
+                }
+                else {
+                    setdata([]);
+                    setisloading(false);
+                    setiserror(true);
+                }
+            }).catch(() => {
+                setisloading(false);
+                setiserror(true);
+            })
+    }, [email]);
+
+    useEffect(() => {
+        if (!value || !email) return;
+        setisloading(true);
+        fetch(`${url}/kyc/trackandtrace`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                bankEmail: email,
+			    userEmail: value,
+            })
+        }).then(response => response.json())
+            .then(res => {
+                if (res.success) {
+                    setdata(res.message);
+                }
+                else {
+                    setdata([]);
+                    setisloading(false);
+                    setiserror(true);
+                }
+            }).catch(() => {
+                setisloading(false);
+                setiserror(true);
+            })
+    }, [value, email]);
+
+    useEffect(() => {
+        if (!email) return;
+        setisloading(true);
+        fetch(`${url}/util/getuserdetails`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                email: email
+            })
+        }).then(response => response.json())
+            .then(res => {
+                if (res.success) {
+                    setbank(res.message.name);
+                }
+                else {
+                    setdata([]);
+                    setisloading(false);
+                    setiserror(true);
+                }
+            }).catch(() => {
+                setisloading(false);
+                setiserror(true);
+            })
+    }, [email]);
+
+    if (isloading) {
+        return (
+            <React.Fragment>
+                <div className='flex items-center justify-center bg-white ma3 br2 vh-75'>
+                    <Loading text="Loading Dashboard" />
+                </div>
+            </React.Fragment>
+        );
+    }
+
+    if (iserror) {
+        return (
+            <React.Fragment>
+                <div className='flex flex-column items-center justify-center bg-white ma3 br2 vh-75'>
+                    <p className="f3 gray">Something Went Wrong!</p>
+                    <button className="mt4 fw6 f6 bn dim br1 ph3 pointer pv2 dib white" style={{ background: "#6EB6FF" }} onClick={() => history.push('/')}>Go Back</button>
+                </div>
+            </React.Fragment>
+        );
+    }
+
+    return (
+        <div>
+            <h2 className="text-center text-gray-700">{`Welcome to ${bank} Admin Dashboard`}</h2>
+            <div className="flex w-full justify-center items-center">
+                <input className="" type="search" onchange={(e) =>setvalue(e.target.value)}/>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2">
+                {
+                    (data.length <= 0 || data === undefined) ? <div className="">No data found !</div>
+                    :
+                    <div className="grid grid-cols-2 md:grid-cols-4">
+                        {
+                            data.map((val, idx) => {
+                                return (
+                                    <div className="rounded-xl m-4 drop-shadow-md bg-white w-10/12 mx-auto">
+                                        <p className="text-gray-600">{val.email}</p>
+                                        <p className="text-gray-600">{val.type_of_transaction}</p>
+                                        <p className="text-gray-600">{val.timestamp}</p>
+                                        <p className="text-gray-600">{val.bank}</p>
+                                    </div>
+                                );
+                            })
+                        }
+                    </div>
+                }
+            </div>
+        </div>
+    )
+}
---
+++ client/admin/src/components/Header.jsx
@@ -0,0 +1,109 @@
+import { Fragment } from 'react'
+import { Popover, Transition } from '@headlessui/react'
+import { MenuIcon, XIcon } from '@heroicons/react/outline'
+
+export default function Header({ loggedin, setloggedin }) {
+    return (
+        <Popover className="relative bg-white">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6">
+                <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+                    <div className="flex justify-start lg:w-0 lg:flex-1">
+                        <a href="/#">
+                            <img
+                                className="h-8 w-auto sm:h-10 mx-auto"
+                                src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                                alt=""
+                            />
+                            <p className="whitespace-nowrap text-base font-medium text-gray-900">Open KYC</p>
+                        </a>
+                    </div>
+                    <div className="-mr-2 -my-2 md:hidden">
+                        <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                            <MenuIcon className="h-6 w-6" aria-hidden="true" />
+                        </Popover.Button>
+                    </div>
+                    {loggedin ?
+                        <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+                            <a
+                                href="/" onClick={() => {localStorage.removeItem('amexloggedin');localStorage.removeItem('email');localStorage.removeItem('bank'); loggedin(false);}}
+                                className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+                            >
+                                Sign Out
+                            </a>
+                        </div>
+                        :
+                        <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+                            <a href="/login" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+                                Sign in
+                            </a>
+                            <a
+                                href="/register"
+                                className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+                            >
+                                Sign up
+                            </a>
+                        </div>
+                    }
+                </div>
+            </div>
+
+            <Transition
+                as={Fragment}
+                enter="duration-200 ease-out"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="duration-100 ease-in"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+            >
+                <Popover.Panel focus className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
+                    <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
+                        <div className="pt-5 pb-6 px-5">
+                            <div className="flex items-center justify-between">
+                                <div>
+                                    <img
+                                        className="h-8 w-auto"
+                                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                                        alt="Workflow"
+                                    />
+                                </div>
+                                <div className="-mr-2">
+                                    <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                                        <span className="sr-only">Close menu</span>
+                                        <XIcon className="h-6 w-6" aria-hidden="true" />
+                                    </Popover.Button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="py-6 px-5 space-y-6">
+                            {loggedin ?
+                                <a
+                                    href="/"
+                                    onClick={() => {localStorage.removeItem('amexloggedin');localStorage.removeItem('email');loggedin(false);localStorage.removeItem('bank');}}
+                                    className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+                                >
+                                    Sign Out
+                                </a>
+                                :
+                                <div>
+                                    <a
+                                        href="/login"
+                                        className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+                                    >
+                                        Sign up
+                                    </a>
+                                    <p className="mt-6 text-center text-base font-medium text-gray-500">
+                                        Existing customer?{' '}
+                                        <a href="/register" className="text-indigo-600 hover:text-indigo-500">
+                                            Sign in
+                                        </a>
+                                    </p>
+                                </div>
+                            }
+                        </div>
+                    </div>
+                </Popover.Panel>
+            </Transition>
+        </Popover>
+    )
+}
