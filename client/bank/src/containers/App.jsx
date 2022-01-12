e610648e54ad38f3dd0f015fb9c01b94054d115b
Pranav Agarwal <p2012agarwal@gmail.com>
2022-01-12 23:42:00 +0530

    fix :)

--- client/bank/src/containers/App.jsx
+++ client/bank/src/containers/App.jsx
@@ -4,10 +4,11 @@ import Routes from '../components/Routes';
 export default function App() {
 
   const [isloggedin, setisloggedin] = useState(JSON.parse(localStorage.getItem('amexloggedin')) || false);     //eslint-disable-next-line
+  const [video_status, setvideo_status] = useState(false);
 
   return (
     <div className="App bg-gray-200 min-h-screen">
-      <Routes loggedin={isloggedin} setloggedin={setisloggedin}/>
+      <Routes loggedin={isloggedin} setloggedin={setisloggedin} video_status={video_status} setvideo_status={setvideo_status}/>
     </div>
   );
 }
